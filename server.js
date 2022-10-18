1d2c6ec718f192093fd7a5849b1e80a155587483
Aingkaran <aingkaran1995@gmail.com>
2022-10-18 19:41:45 -0400

    Revise App and Server files

--- server.js
+++ server.js
@@ -10,31 +10,10 @@ console.log(NbaTeams.Hornets.id)
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/hello', (req, res) => {
+app.get('/api', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
-app.post('/api/world', (req, res) => {
-    axios.get(`http://api.sportradar.us/nba/trial/v7/en/teams/${NbaTeams.Raptors.id}/profile.json?api_key=c83bmnmqk6rphqcyetq3r928`)
-    .then((response=>{
-        let Team =[]
-
-        for (let i=0;i<(response.data.players).length;i++){
-            console.log(response.data.players[i]);
-            Team.push(response.data.players[i])
-        }
-        res.send(
-            `Player: ${Team}  `,
-          );
-    }))
-    .catch((err)=>{
-        console.log(err);
-    })
 
   
-});
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
