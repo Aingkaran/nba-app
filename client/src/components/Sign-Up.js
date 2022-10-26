042310358ba218ee32d375a80ea5cf53b1fe1131
Aingkaran <aingkaran1995@gmail.com>
2022-10-25 22:08:00 -0400

    Continue user Auth

--- client/src/components/Sign-Up.js
+++ client/src/components/Sign-Up.js
@@ -1,34 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Signup =()=>{
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [registerFirstName, setRegisterFirstName] = useState("");
-  const [registerLastName, setRegisterLastName] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
+  const [data, setData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+	});
 
 
-  const register = () => {
-    axios({
-      method: "POST",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-        firstname: registerFirstName,
-        lastname: registerLastName,
-        email: registerEmail,
-      },
-      withCredentials: true,
-      url: "http://localhost:5000//api/users",
-    }).then((res) => console.log(res));
-  };
+  const handleChange = ({ currentTarget: input }) => {
+		setData({ ...data, [input.name]: input.value });
+	};
+
+  const [error, setError] = useState("");
+
+	const handleSubmit = async (e) => {
+    console.log(data)
+		e.preventDefault();
+		try {
+			const url = "http://localhost:5000/api/users";
+			const { data: res } = await axios.post(url, data);
+      console.log(data)
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				setError(error.response.data.message);
+			}
+		}
+	};
 
 
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Sign Up</h3>
         <div className="mb-3">
           <label>First name</label>
@@ -36,7 +48,9 @@ const Signup =()=>{
             type="text"
             className="form-control"
             placeholder="First name"
-            onChange={(e) => setRegisterFirstName(e.target.value)}
+            onChange={handleChange}
+            name="firstName"
+            value={data.firstName}
           />
         </div>
         <div className="mb-3">
@@ -45,7 +59,9 @@ const Signup =()=>{
             type="text" 
             className="form-control" 
             placeholder="Last name" 
-            onChange={(e) => setRegisterLastName(e.target.value)} />
+            onChange={handleChange} 
+            name="lastName"
+            value={data.lastName}/>
         </div>
         <div className="mb-3">
           <label>Email address</label>
@@ -53,7 +69,9 @@ const Signup =()=>{
             type="email"
             className="form-control"
             placeholder="Enter email"
-            onChange={(e) => setRegisterEmail(e.target.value)} 
+            onChange={handleChange} 
+            name="email"
+            value={data.email}
             
           />
         </div>
@@ -63,11 +81,13 @@ const Signup =()=>{
             type="password"
             className="form-control"
             placeholder="Enter password"
-            onChange={(e) => setRegisterPassword(e.target.value)} 
+            onChange={handleChange} 
+            name="password"
+            value={data.password}
           />
         </div>
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary" onClick={register}>
+          <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
             Sign Up
           </button>
         </div>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const NbaTeams= require('./routes/NBATeams')
 const cors = require('cors')
+const User = require('./models/user')
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
@@ -8,11 +9,19 @@ const Schema = mongoose.Schema;
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 
+mongoose.connect(
+    "mongodb+srv://NBA-APP:Aingkaran@nba-app.douk6cs.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+      console.log("Mongoose Is Connected");
+    }
+  );
 
 
 
-const mongoDb = "mongodb+srv://NBA-APP:Aingkaran@nba-app.douk6cs.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
@@ -21,10 +30,14 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 
 
-
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(
+    cors({
+      origin: "http://localhost:3000", // <-- location of the react app were connecting to
+      credentials: true,
+    })
+  );
 app.use('/', NbaTeams);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
